5295ecbe42252558304d4b8261a20d6c95438f78
Serhii-Pashchenko <paschenko09@gmail.com>
2023-05-20 15:50:06 +0300

    Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import MovieBox from './components/MovieBox';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Container, Nav, Form, FormControl, Button } from 'react-bootstrap';
+
+const API_URL = "https://api.themoviedb.org/3/movie/popular?api_key=e4016d7a28f89745bc184dfb96de3b98";
+const API_SEARCH = "https://api.themoviedb.org/3/search/movie?api_key=e4016d7a28f89745bc184dfb96de3b98&query=";
+const API_GENRES = "https://api.themoviedb.org/3/genre/movie/list?api_key=e4016d7a28f89745bc184dfb96de3b98";
 
 function App() {
+
+  const [movies, setMovies] = useState([]);
+  const [genres, setGenre] = useState([]);
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    fetch(API_URL)
+    .then(response => response.json())
+    .then(data => {
+      setMovies(data.results)
+    })
+    .catch(error => console.log(error));
+
+    fetch(API_GENRES)
+    .then(response => response.json())
+    .then(data => {
+      setGenre(data.genres);
+    })
+    .catch(error => console.log(error));
+  }, []);
+
+  const searchMovie = async (e) => {
+    e.preventDefault();
+    try {
+      await fetch(API_SEARCH + query)
+      .then(response => response.json())
+      .then(data => {
+        setMovies(data.results)
+      })
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  const changeHandler = (e) => {
+    setQuery(e.target.value);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar bg="dark" expand="lg" variant="dark">
+        <Container fluid>
+          <Navbar.Brand href="/home">Movie App</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll"></Navbar.Toggle>
+            <Navbar.Collapse id="navbarScroll">
+              <Nav className="me-auto my-2 my-lg-3" style={{ maxHeight: '100px' }} navbarScroll></Nav>
+              <Form className="d-flex" onSubmit={searchMovie} autoComplete="off">
+                <FormControl
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                  name = "query"
+                  value={query}
+                  onChange={changeHandler}></FormControl>
+                <Button variant="secondary" type="submit">Search</Button>
+              </Form>
+            </Navbar.Collapse>
+          
+        </Container>
+      </Navbar>
+      <div>
+        {movies.length > 0 ? (
+          <div className="container">
+            <div className="grid">
+              {movies.map((movieReq) => 
+                <MovieBox key={movieReq.id} {...movieReq} genres={genres}/>
+              )}
+            </div>
+          </div>
+        ) : (
+          <h2> Sorry, no movies found :( </h2>
+        )}
+        
+      </div>
     </div>
   );
 }
---
+++ src/components/MovieBox.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from "react";
+import { Modal, show, Button } from "react-bootstrap";
+const API_IMG = "https://image.tmdb.org/t/p/w500/";
+
+const MovieBox = ({id, title, poster_path, genre_ids, vote_average, release_date, overview, genres}) => {
+  const [show, setShow] = useState(false);
+  const [actors, setActors] = useState([]);
+
+  const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+
+  useEffect(() => {
+    fetch(`http://api.themoviedb.org/3/movie/${id}/casts?api_key=e4016d7a28f89745bc184dfb96de3b98`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data.cast);
+      setActors(data.cast);
+    })
+    .catch(error => console.log(error));
+  }, []);
+
+  return (
+    <div className="card text-center bg-secondary mb-3 mt-3">
+      <div className="card-body">
+        <img className="card-img-top" src={API_IMG + poster_path} alt={title} />
+        <div style={{paddingBottom:'0'}} className="card-body">
+          <button type="button" className="btn btn-dark" onClick={handleShow}> View Details </button>
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title></Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <img className="card-img-top" src={API_IMG + poster_path} />
+              <h3>{title}</h3>
+              <h4>IMDb: {vote_average}</h4>
+              <h5>Genres: {genre_ids.map(genreId => {
+                const genre = genres.find(g => g.id === genreId);
+                return genre ? genre.name : '';
+              }).join(', ')}</h5>
+              <h5>Release Date: {release_date}</h5>
+              <br></br>
+              <h5>Overview</h5>
+              <p>{overview}</p>
+              <h5>Actors</h5><br/>
+              <div className="actors">
+                {actors.map(actor => (
+                  <div className="actors item" key={actor.id}>
+                    <img style={{width:'4rem'}} src={API_IMG + actor.profile_path} />
+                    <div>
+                      <span style={{fontWeight:'bold'}}>{actor.name}</span><br/>
+                      <span style={{fontStyle:'italic', color:'grey'}}>{actor.character}</span>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>Close</Button>
+            </Modal.Footer>
+          </Modal>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MovieBox;
